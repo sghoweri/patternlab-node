140ecff77bdb99918149115dc8d2cd70228f8b27
Salem Ghoweri <salem.ghoweri@pega.com>
2018-11-26 09:01:40 -0500

    feat: polish up UI for tools menu + add SVG icons to menu items; add react popper tooltip as a quick tooltip UI implementation

---
+++ packages/uikit-workshop/src/scripts/components/tooltip/tooltip.js
@@ -0,0 +1,36 @@
+import TooltipTrigger from 'react-popper-tooltip';
+import { h } from 'preact';
+import 'react-popper-tooltip/dist/styles.css';
+
+export const Tooltip = ({ tooltip, children, hideArrow, ...props }) => (
+  <TooltipTrigger
+    {...props}
+    tooltip={({
+      getTooltipProps,
+      getArrowProps,
+      tooltipRef,
+      arrowRef,
+      placement,
+    }) => (
+      <div
+        {...getTooltipProps({
+          ref: tooltipRef,
+          className: 'tooltip-container',
+        })}
+      >
+        {!hideArrow && (
+          <div
+            {...getArrowProps({
+              ref: arrowRef,
+              'data-placement': placement,
+              className: 'tooltip-arrow',
+            })}
+          />
+        )}
+        {tooltip}
+      </div>
+    )}
+  >
+    {children}
+  </TooltipTrigger>
+);
